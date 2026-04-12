0b42f8c1e34aa37376342224154c40869eca4c85
Guilherme Castilho Machado <guiguicastilho34@gmail.com>
2026-04-12 13:40:40 -0300

    chore: cria estrutura inicial da api de tickets de suporte

---
+++ src/main/java/br/ufu/apiticketssuporte/ApiDeTicketsDeSuporteApplication.java
@@ -0,0 +1,13 @@
+package br.ufu.apiticketssuporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeTicketsDeSuporteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDeTicketsDeSuporteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ufu/apiticketssuporte/ApiDeTicketsDeSuporteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufu.apiticketssuporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeTicketsDeSuporteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
