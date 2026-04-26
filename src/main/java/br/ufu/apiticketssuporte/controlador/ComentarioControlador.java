750b04c0c2c866937923b86aa2ebbe44db8316b3
Guilherme Castilho Machado <guiguicastilho34@gmail.com>
2026-04-26 19:51:23 -0300

    feat: implementa cadastro e listagem de comentarios por chamado

--- src/main/java/br/ufu/apiticketssuporte/controlador/ComentarioControlador.java
+++ src/main/java/br/ufu/apiticketssuporte/controlador/ComentarioControlador.java
@@ -1,4 +1,30 @@
 package br.ufu.apiticketssuporte.controlador;
 
+import br.ufu.apiticketssuporte.modelo.Comentario;
+import br.ufu.apiticketssuporte.servico.ComentarioServico;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chamados/{chamadoId}/comentarios")
+@RequiredArgsConstructor
 public class ComentarioControlador {
+
+    private final ComentarioServico comentarioServico;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comentario adicionarComentario(@PathVariable Long chamadoId,
+                                          @RequestBody @Valid Comentario comentario) {
+        return comentarioServico.adicionarComentario(chamadoId, comentario);
+    }
+
+    @GetMapping
+    public List<Comentario> listarComentariosPorChamado(@PathVariable Long chamadoId) {
+        return comentarioServico.listarComentariosPorChamado(chamadoId);
+    }
 }
--- src/main/java/br/ufu/apiticketssuporte/servico/ComentarioServico.java
+++ src/main/java/br/ufu/apiticketssuporte/servico/ComentarioServico.java
@@ -1,4 +1,31 @@
 package br.ufu.apiticketssuporte.servico;
 
+import br.ufu.apiticketssuporte.modelo.Chamado;
+import br.ufu.apiticketssuporte.modelo.Comentario;
+import br.ufu.apiticketssuporte.repositorio.ComentarioRepositorio;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ComentarioServico {
+
+    private final ComentarioRepositorio comentarioRepositorio;
+    private final ChamadoServico chamadoServico;
+
+    public Comentario adicionarComentario(Long chamadoId, Comentario comentario) {
+        Chamado chamado = chamadoServico.buscarChamadoPorId(chamadoId);
+
+        comentario.setId(null);
+        comentario.setChamado(chamado);
+
+        return comentarioRepositorio.save(comentario);
+    }
+
+    public List<Comentario> listarComentariosPorChamado(Long chamadoId) {
+        chamadoServico.buscarChamadoPorId(chamadoId);
+        return comentarioRepositorio.findByChamadoIdOrderByDataCriacaoAsc(chamadoId);
+    }
 }
